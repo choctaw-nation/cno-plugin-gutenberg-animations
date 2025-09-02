defb2ba1baef7cbec345cdd9c281ff68c1b928af
KJ Roelke <kroelke@choctawnation.com>
2025-09-02 13:42:22 -0500

    add missing package

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-import defaultConfig from '@wordpress/scripts/config/webpack.config.js'
+import defaultConfig from '@wordpress/scripts/config/webpack.config.js';
 import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts';
 
-
 const config = {
-		...defaultConfig,
-		entry: {
-			...defaultConfig.entry(),
-			view: `./src/view/animation-fallback.ts`
-		},
-		plugins: [
-			...defaultConfig.plugins,
-			new RemoveEmptyScriptsPlugin( {
-				stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
-			} ),
-		],
+	...defaultConfig,
+	entry: {
+		...defaultConfig.entry(),
+		view: `./src/view/animation-fallback.ts`,
+	},
+	plugins: [
+		...defaultConfig.plugins,
+		new RemoveEmptyScriptsPlugin( {
+			stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
+		} ),
+	],
 };
 export default config;
